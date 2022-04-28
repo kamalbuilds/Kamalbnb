af5b54f80e45176fa73dce48bed0df300a470223
Kamal Nayan <geniusamansingh@gmail.com>
2022-04-28 07:49:17 +0000

    creating schemas

--- kamalbnb-backend/schemas/schema.js
+++ kamalbnb-backend/schemas/schema.js
@@ -3,7 +3,8 @@ import createSchema from 'part:@sanity/base/schema-creator'
 
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
-
+import person from './person';
+import property from './property';
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
   // We name our schema
@@ -12,5 +13,11 @@ export default createSchema({
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
     /* Your types here! */
+    property,
+    // propertyImage,
+    // review,
+    // traveller,
+    // host,
+    // person,
   ]),
 })
