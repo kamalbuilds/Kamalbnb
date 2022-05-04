2daf60468fd1e4969ed20e063f0fde4f0e5c4475
Kamal Nayan <95926324+legendarykamal@users.noreply.github.com>
2022-05-05 01:20:35 +0530

    Update Image.js

--- kamalbnb-frontend/components/Image.js
+++ kamalbnb-frontend/components/Image.js
@@ -1,7 +1,7 @@
 import { urlFor } from "../sanity"
 import Image from "next/image"
 
-const Image = ({ identifier, image }) => {
+const Image2 = ({ identifier, image }) => {
   return (
     <div className={identifier === "main-image" ? "main-image" : "image"}>
       <Image src={urlFor(image).auto("format")} />
@@ -9,4 +9,4 @@ const Image = ({ identifier, image }) => {
   )
 }
 
-export default Image
+export default Image2
